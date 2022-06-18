2372d96b78f345d42dc3b0faa080ae209acfbce5
jepariz <je.parizotto@gmail.com>
2022-06-18 12:33:19 -0300

    criei o estilo da virada das cartas

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-let qtdCartas = prompt(`Bem-vindo ao Parrot Card Game! 
+let qtdCartas = Number((prompt(`Bem-vindo ao Parrots Card Game! 
 Escolha a quantidade de cartas entre 4 e 14. 
 Lembre-se que o número de cartas precisa ser par.
 
-Com quantas cartas você quer jogar?`);
+Com quantas cartas você quer jogar?`)));
 
-if(qtdCartas % 2 != 0 ){
-    alert("Você deve escolher um número par de cartas!")
-}
+
+
+// while(qtdCartas%2 != 0){
+//     alert("Você deve escolher um número par entre 4 e 14!");
+//     prompt("Com quantas cartas você quer jogar?");
+//     qtdCartas = qtdCartas%2 == 0;
+// } 
+
+let listaCartas = ['', ]
+
+
+
+const cartas = document.querySelector(".atras");
