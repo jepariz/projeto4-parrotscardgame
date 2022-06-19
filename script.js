88eb0a627ff1ae9c07efbbb164af65574c36ee6b
jepariz <je.parizotto@gmail.com>
2022-06-18 22:43:13 -0300

    criei o loop para o usuário escolher um número par

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
-const cards = document.querySelectorAll(".card");
+let numero = prompt(`Bem-vindo(a) ao Parrots Card Game
+Escolha o número de cartas entre 4 e 14
+
+Lembre-se que o número deve ser par!` );
+
+let num = Number(numero);
+
+let contador = 0;
+
+if(num%2 == 0 && num <= 14 && num >=4){
+    alert("Pode jogar")
+
+} else{
+
+while(num%2 !== 0 || num >14 || num < 4){
+    alert("Número inválido! Por favor, digite um número par entre 4 e 14");
+    num = prompt("Digite o novo número")
+    contador ++;
+} 
+}
+console.log("Acabou");
 
 
 
 
 
 
-// let qtdCartas = Number((prompt(`Bem-vindo ao Parrots Card Game! 
-// Escolha a quantidade de cartas entre 4 e 14. 
-// Lembre-se que o número de cartas precisa ser par.
-
-// Com quantas cartas você quer jogar?`)));
 
 
 
-// while(qtdCartas%2 != 0){
-//     alert("Você deve escolher um número par entre 4 e 14!");
-//     prompt("Com quantas cartas você quer jogar?");
-//     qtdCartas = qtdCartas%2 == 0;
-// } 
+// const cards = document.querySelectorAll(".card");
+// let cartaClicada;
+
+// function clickedCard(element){
+//     cartaClicada = document.querySelector(".card");
+//     console.log(cartaClicada)
+
+//     if(cartaClicada !== null){
+//         cartaClicada.classList.remove("flip");
+//     }
+//     cartaClicada.classList.add("flip");
+// }
+
+
 
 
 
