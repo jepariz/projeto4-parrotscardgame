2205b935b3b8b9ee508f6bcb95588618edcd1f29
jepariz <je.parizotto@gmail.com>
2022-06-20 18:07:31 -0300

    arrumei o seletor de cartas

--- script.js
+++ script.js
@@ -1,86 +1,87 @@
-
-const cartas = ['./img/bobrossparrot.gif', 
-'./img/explodyparrot.gif',
-'./img/fiestaparrot.gif',
-'./img/metalparrot.gif',
-'./img/revertitparrot.gif',
-'./img/tripletsparrot.gif',
-'./img/unicornparrot.gif',
-'./img/bobrossparrot.gif', 
-'./img/explodyparrot.gif',
-'./img/fiestaparrot.gif',
-'./img/metalparrot.gif',
-'./img/revertitparrot.gif',
-'./img/tripletsparrot.gif',
-'./img/unicornparrot.gif',
-];
-
-cartas.sort(comparador); 
-
-function comparador() { 
-    return Math.random() - 0.5; 
-}
-
-
-let numero = prompt(
-`Bem-vindo(a) ao Parrots Card Game
-Escolha o número de cartas entre 4 e 14
-
-Lembre-se que o número deve ser par!` );
-
-let num = Number(numero);
-
-let incremento = 0;
-
-if(num%2 == 0 && num <= 14 && num >=4){
+const cartas = [
+    "bobross",
+    "explody",
+    "fiesta",
+    "metal",
+    "revertit",
+    "triplets",
+    "unicorn"
+  ];
+  
+  let numero = 8;
+  /* prompt(
+    `Bem-vindo(a) ao Parrots Card Game
+  Escolha o número de cartas entre 4 e 14
+  
+  Lembre-se que o número deve ser par!`
+  ); */
+  
+  // Aviso para o usuário escolher o número de cartas
+  
+  let incremento = 0;
+  
+  let tabuleiro = [];
+  
+  let quantidadeCartas = numero / 2;
+  
+  function comparador() {
+    return Math.random() - 0.5;
+  }
+  let cartaSorteada = cartas.sort(comparador);
+  
+  if (numero % 2 === 0 && numero <= 14 && numero >= 4) {
     insereCartas();
-
- } else{
-
-while(num%2 !== 0 || num >14 || num < 4){
-    alert("Número inválido! Por favor, digite um número par entre 4 e 14");
-    num = prompt("Digite o novo número")
-    incremento ++;
-    insereCartas();
-} 
-} 
-
-function insereCartas (){
-    
-    for(let index = 0; index < num; index ++){
-        const cartaTemplate = `
-        <div class="card" data-identifier="card">
-        <div class="back-face" data-identifier="back-face">
-            <img src="${cartas[index]}">
-        </div>
-        <div class="front-face" onclick="virarCarta(this); estaVirada(this)" data-identifier="front-face">
-            <img src="/img/front.png">
-        </div>
-    </div>
-        `
-        document.querySelector('.container').innerHTML += cartaTemplate;
-
+  } else {
+    while (numero % 2 !== 0 || numero > 14 || numero < 4) {
+      alert("Número inválido! Por favor, digite um número par entre 4 e 14");
+      numero = prompt("Digite o novo número");
+      incremento++;
+      insereCartas();
     }
-}
+  }
+  
+  //Funcões que pegam as cartas que serão colocadas no tabuleiro
+  
+  function insereCartas() {
+    for (let index = 0; index < quantidadeCartas; index++) {
+      let cartaSelecionada = cartaSorteada;
+      tabuleiro.push(cartaSelecionada[index]);
+      tabuleiro.push(cartaSelecionada[index]);
+    }
+  
+    tabuleiro = tabuleiro.sort(comparador);
+  
+    for (let index = 0; index < numero; index++) {
+      const cartaTemplate = `
+  <div class="card" data-identifier="card">
+  <div class="back-face" data-identifier="back-face">
+  <img src="./img/${tabuleiro[index]}parrot.gif">
+  </div>
+  <div class="front-face" onclick="virarCarta(this); "compararCartas(this)" data-identifier="front-face">
+  <img src="/img/front.png">
+  </div>
+  </div>
+  `;
+      document.querySelector(".container").innerHTML += cartaTemplate;
+    }
+  }
+  
+//Funcões de virar cartas e comparar
 
+    let hasFlippedCard = false;
+    let firstCard, secondCard;
 
-function virarCarta(elemento){
-    const pai = elemento.parentNode;
-    pai.classList.add("flip");
-}
+  function virarCarta(elemento) {
+    let pai = elemento.parentNode;
+    pai.classList.add('flip');
 
-let jogadas = 0;
-let primeiraCarta;
-let segundaCarta;
-
-function estaVirada(elemento){
-    const pai = document.querySelector(".front-face").parentNode;
-    const virada = pai.classList.contains('flip');
-    console.log(virada)
-
-    
-}
-estaVirada();
-
-
-
+   if (!hasFlippedCard) {
+     hasFlippedCard = true;
+     firstCard = this;
+     return
+  } 
+  else{
+    secondCard = this;
+    hasFlippedCard = false;
+    }
+  }
