ad050e6dde8ca07cbddd77ea98ed3c9ef18ee0e7
jepariz <je.parizotto@gmail.com>
2022-08-17 13:08:18 -0300

    criei a função de virar as cartas

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 let numero;
 let tabuleiro;
+let cartaSelecionada;
+let primeiraCarta, segundaCarta;
 
 const cartas = [
-    'bobrossparrot.gif', 
-    'explodyparrot.gif', 
-    'fiestaparrot.gif', 
-    'metalparrot.gif', 
-    'revertitparrot.gif', 
-    'tripletsparrot.gif', 
-    'unicornparrot.gif'
+    'bobrossparrot', 
+    'explodyparrot', 
+    'fiestaparrot', 
+    'metalparrot', 
+    'revertitparrot', 
+    'tripletsparrot', 
+    'unicornparrot'
 ];
 
 let cartasEmbaralhadas = [];
+
 
 
 function comparador() { 
@@ -25,9 +28,9 @@ function inserirCartas(){
     
    for(let i = 0; i < cartasEmbaralhadas.length; i++ ){
     let modeloCarta = `
-    <div class="carta">
-        <img class="front" src="./img/front.png">
-        <img class="back" src="./img/${cartasEmbaralhadas[i]}">
+    <div class="carta" >
+        <img onclick="selecionarCarta(this)" class="front ${cartasEmbaralhadas[i]}" src="./img/front.png">
+        <img  class="back" src="./img/${cartasEmbaralhadas[i]}.gif">
     </div>`
 
     tabuleiro.innerHTML = tabuleiro.innerHTML + modeloCarta;
@@ -50,7 +53,8 @@ function iniciarJogo(){
 // Escolher número de cartas -------------------------------------------------------------------------------
 
 function definirNumeroCartas(){
-    numero = Number(prompt(`Com quantas cartas você quer jogar? OBS: Escolha um número PAR entre 4 e 14`));
+    numero = 4;
+    // numero = Number(prompt(`Com quantas cartas você quer jogar? OBS: Escolha um número PAR entre 4 e 14`));
     while(numero % 2 !== 0 || numero === null || numero === NaN || numero < 4 || numero > 14){
         alert('Por favor, escolha um número PAR entre 4 e 14');
         numero = prompt("Digite o número escolhido");
@@ -60,10 +64,35 @@ definirNumeroCartas();
 iniciarJogo();
 
 
+function selecionarCarta(carta){
 
+    cartaSelecionada = carta.parentNode;
+    cartaSelecionada.classList.add("flip");
 
+    // cartaSelecionada1 = carta.parentNode;
+    // cartaSelecionada1.classList.add("flip");
 
+    console.log(cartaSelecionada)
 
+    if(primeiraCarta === undefined){
+       primeiraCarta = carta.classList[1];;
+    } else if(segundaCarta === undefined){
+        segundaCarta = carta.classList[1];
+    }
+
+    // if(primeiraCarta !== undefined && segundaCarta !== undefined && segundaCarta !== primeiraCarta){
+
+    //     // function desvirarCarta(){
+    //     // cartaSelecionada.classList.add("shake");
+    //     // cartaSelecionada.classList.remove("flip");
+
+    //     // }
+
+    // }
+
+   console.log(primeiraCarta,segundaCarta)
+
+}
 
 
 
